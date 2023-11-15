cf083e2f0727e1cff44b3471e7ba86f970d4ad19
David Pulec <pulec@spsejecna.cz>
2023-11-15 08:49:52 +0100

    GG

---
+++ script.js
@@ -0,0 +1,35 @@
+function plus(a, b){
+    return a + b;
+}
+function minus(a, b){
+    return a - b;
+}
+function krat(a, b){
+    return a * b;
+}
+function deleni(a, b){
+    return a / b;
+}
+
+function sin(a){
+    return Math.sin(a);
+}
+function cos(a){
+    return Math.cos(a);
+}
+function tg(a){
+    return Math.tan(a);
+}
+function cotg(a){
+    return 1/Math.tan(a);
+}
+
+function log(a){
+    return Math.log(a);
+}
+function exp(a){
+    return Math.exp(a);
+}
+function sqr(a){
+    return Math.sqrt(a);
+}
